7ebec372e4625d21a86adb4390f0fa2e0364632c
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-01-13 22:51:16 +0300

    12312

--- src/com/company/Lesson1/Task2.java
+++ src/com/company/Lesson1/Task2.java
@@ -18,8 +18,8 @@ public class Task2 {
     где a, b, c, d – аргументы этого метода, имеющие тип float.
     */
 
-        System.out.println(calcExp(12f,13f,14f,15f));
-        System.out.println(calcExp(12.32f,13.333f,14.321f,15.321321f));
+        System.out.println(calcExp(12f, 13f, 14f, 15f));
+        System.out.println(calcExp(12.32f, 13.333f, 14.321f, 15.321321f));
 /*
      4.   Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
      где a, b, c, d – аргументы этого метода, имеющие тип float.
@@ -63,64 +63,70 @@ public class Task2 {
         calcLeapYear(96);
         calcLeapYear(32);
         calcLeapYear(400);
-        calcLeapYear(1200);
-        calcLeapYear(1100);
-    }
-    /**
-     *Метод вычисляющий выражение вида a *(b + (c/d))
-     * @return float результат выражения
-    */
-    static float calcExp(float a,float b,float c, float d){
-        return a *(b + (c/d));
+        calcLeapYear(1900);
+        calcLeapYear(2000);
     }
 
     /**
-     *Метод проверяющий выходит ли число в промежуток между 10 и 20
+     * Метод вычисляющий выражение вида a *(b + (c/d))
+     *
+     * @return float результат выражения
+     */
+    static float calcExp(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+
+    /**
+     * Метод проверяющий выходит ли число в промежуток между 10 и 20
+     *
      * @param num целоечисло
      * @return boolean false/true
      */
-    static  boolean checkIntervalNum(int num){
+    static boolean checkIntervalNum(int num) {
         return num <= 20 && num >= 10;
     }
 
     /**
      * Программа вычисляет положительное или отричательное число
+     *
      * @param num целове число
      * @return String возращает сообщение о том что число положительное или отрицательное
-     *
      */
-    static void printPositiveOrNegativeNum(int num){
-        if(num >= 0) {
+    static void printPositiveOrNegativeNum(int num) {
+        if (num >= 0) {
             System.out.println("Число положительное");
             return;
         }
         System.out.println("Число отрицательное");
     }
+
     /**
      * Программа вычисляет отрицательное число
+     *
      * @param num целове число
      * @return String возращает true, если число отрицательное
-     *
      */
-    static boolean checkNegativeNum(int num){
+    static boolean checkNegativeNum(int num) {
         return num < 0;
     }
 
     /**
-     *Выводит приветствие определенного человека
+     * Выводит приветствие определенного человека
+     *
      * @param name имя которое необходимо напечатать
      * @return печатает имя в консоли
      */
-    static  void printHelloName(String name){
+    static void printHelloName(String name) {
         System.out.println("Привет " + name);
     }
 
     /**
      * Вычисляет високосный год
+     *
      * @param year год
      */
-    static void calcLeapYear(int year){
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+    static void calcLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год високосный");
             return;
         }
