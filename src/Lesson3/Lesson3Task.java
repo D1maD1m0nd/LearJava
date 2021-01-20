ef1b86ff586fbbeb4de1d6565b3db58b732a944a
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-01-20 21:34:22 +0300

    Конец игры угадай число

--- src/Lesson3/Lesson3Task.java
+++ src/Lesson3/Lesson3Task.java
@@ -9,7 +9,7 @@ public class Lesson3Task {
         //Написать программу, которая загадывает случайное число от 0 до 9 и пользователю дается 3 попытки угадать это число. При каждой попытке компьютер должен сообщить,
         // больше ли указанное пользователем число, чем загаданное, или меньше.
         // После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
-
+        startGameGuessNumber(10);
 
     }
     public static int getRandomNum(int period){
@@ -18,7 +18,7 @@ public class Lesson3Task {
     public static int getUserAnswerGuessNum(int period){
         int usrNum;
         do{
-            System.out.printf("Введите число от %d до %d или введите -1 для выхода: ", 0,period);
+            System.out.printf("Введите число от %d до %d или введите -1 для выхода: ", 0,period - 1);
             Scanner usr = new Scanner(System.in);
             usrNum = usr.nextInt();
             if(usrNum == -1) return -1;
@@ -28,11 +28,15 @@ public class Lesson3Task {
     }
     public static int getUserAnswerContinueGame(){
         Scanner usr = new Scanner(System.in);
-        String usrAnswer = usr.next();
-        if(usrAnswer.toLowerCase(Locale.ROOT).equals("да")){
+        int usrAnswer = usr.nextInt();
+        if(usrAnswer == 1){
             return 0;
         }
+        if(usrAnswer == 0){
+            return -1;
+        }
         return -1;
+
     }
     public static int checkLessMoreNum(int numUsr, int numHide){
         if(numUsr == numHide) {
@@ -57,26 +61,26 @@ public class Lesson3Task {
         int guessNum = getRandomNum(period);
         do {
             tryCount++;
+
             int usrAnswer = getUserAnswerGuessNum(period);
             result = checkLessMoreNum(usrAnswer,guessNum);
+            if(tryCount == 3 && result != 0){
+                System.out.printf("К сожалению вы проиграли.\nЗагаданное число было %d если желаете поиграть снова," +
+                        " то введите 1, в ином случае введите 0 ", guessNum);
+
+                gameContinue = true;
+            }
             if(result == 0){
-                System.out.printf("Поздравляю вы справились за %d попыток, если желаете поиграть снова, то введите ДА, в ином случае введите НЕТ или любые символы", tryCount);
-                if(getUserAnswerContinueGame() == -1){
-                    System.out.println("Игра окончена!");
-                    break;
-                }
-                gameContinue = true;
-                guessNum = getRandomNum(period);
-            }
-            if(tryCount == 3){
-                System.out.println("К сожалению вы проиграли, если желаете поиграть снова, то введите ДА, в ином случае введите НЕТ или любые символы");
-                if(getUserAnswerContinueGame() == -1){
-                    System.out.println("Игра окончена!");
-                    break;
-                }
+                System.out.printf("Поздравляю вы справились за %d попыток, если желаете поиграть снова, то введите 1, в ином случае введите 0 ", tryCount);
+
                 gameContinue = true;
             }
+
             if(gameContinue){
+                if(getUserAnswerContinueGame() == -1){
+                    System.out.println("Игра окончена!");
+                    break;
+                }
                 guessNum = getRandomNum(period);
                 tryCount = 0;
                 gameContinue = false;
