4e424db28c8f9e72cc794ac137881443560d1709
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-01-22 18:06:16 +0300

    исправление форматирования

--- src/Lesson3/Lesson3Task2.java
+++ src/Lesson3/Lesson3Task2.java
@@ -5,31 +5,32 @@ import java.util.Scanner;
 
 public class Lesson3Task2 {
     public static void main(String[] args) {
-/*
- *Создать массив из слов
- *  String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
- *  "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
- * "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"}. При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
- * сравнивает его с загаданным словом и сообщает, правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы, которые стоят
- * на своих местах. apple – загаданное apricot - ответ игрока ap############# (15 символов, чтобы пользователь не мог узнать длину слова) Для сравнения
- * двух слов посимвольно можно пользоваться:
- * String str = "apple"; char a = str.charAt(0); - метод, вернет char,
- * который стоит в слове str на первой позиции Играем до тех пор, пока игрок не отгадает слово. Используем только маленькие буквы.
- */
+        /*
+         *Создать массив из слов
+         *  String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+         *  "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+         * "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"}. При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+         * сравнивает его с загаданным словом и сообщает, правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы, которые стоят
+         * на своих местах. apple – загаданное apricot - ответ игрока ap############# (15 символов, чтобы пользователь не мог узнать длину слова) Для сравнения
+         * двух слов посимвольно можно пользоваться:
+         * String str = "apple"; char a = str.charAt(0); - метод, вернет char,
+         * который стоит в слове str на первой позиции Играем до тех пор, пока игрок не отгадает слово. Используем только маленькие буквы.
+         */
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
-                "carrot","cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
+                "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
                 "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
         startGameGuessWord(words);
     }
 
     /**
-     *Выбирает случайное слово из коллекции
+     * Выбирает случайное слово из коллекции
+     *
      * @param arr массив слов
      * @return возвращает строку содержащую слово из массива, который был передан в парметре, в случае пустого массива возвращает -1
      */
-    public static String getRandomWordIntoArray(String[] arr){
-        if(arr.length == 0){
+    public static String getRandomWordIntoArray(String[] arr) {
+        if (arr.length == 0) {
             return "-1";
         }
         return arr[new Random().nextInt(arr.length)];
@@ -37,16 +38,17 @@ public class Lesson3Task2 {
 
     /**
      * Производит сопоставление букв в загаданном и пользовательском слове
-     * @param usrWord слово пользователя
+     *
+     * @param usrWord   слово пользователя
      * @param guessWord загаданное слово
      */
-    public static void printPartGuessWord(String usrWord, String guessWord){
-        char[] mask = new char[]{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'};
-        int usrWordLen = Math.min(usrWord.length(),guessWord.length());
+    public static void printPartGuessWord(String usrWord, String guessWord) {
+        char[] mask = new char[]{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};
+        int usrWordLen = Math.min(usrWord.length(), guessWord.length());
         char usrSymbol;
         for (int i = 0; i < usrWordLen; i++) {
             usrSymbol = guessWord.charAt(i);
-            if(usrWord.charAt(i) == usrSymbol){
+            if (usrWord.charAt(i) == usrSymbol) {
                 mask[i] = usrSymbol;
 
             }
@@ -56,9 +58,10 @@ public class Lesson3Task2 {
 
     /**
      * Метод начала игры
+     *
      * @param arr массив слов
      */
-    public static void startGameGuessWord(String[] arr){
+    public static void startGameGuessWord(String[] arr) {
         String randomWord = getRandomWordIntoArray(arr);
         Scanner scan = new Scanner(System.in);
         String usrAnswer;
@@ -67,12 +70,12 @@ public class Lesson3Task2 {
             System.out.print("Введите загаданное слово: ");
             usrAnswer = scan.next();
 
-            if( usrAnswer.equals(randomWord)){
+            if (usrAnswer.equals(randomWord)) {
                 System.out.println("Поздравляю вы победили");
                 break;
             }
             System.out.println("Увы, но вы не угадали");
-            printPartGuessWord(usrAnswer,randomWord);
-        }while (true);
+            printPartGuessWord(usrAnswer, randomWord);
+        } while (true);
     }
 }
---
+++ src/tests/lexa.java
@@ -0,0 +1,243 @@
+package tests;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class lexa {
+    public static void main(String[] args) {
+
+        //для отладки будем показывать задуманное число/слово
+        //для "продакшн" можно выключить и не будет выводиться
+        boolean showSecret = true;
+
+        //столкнулся с ситуацией: если сканер создавать и закрывать в каждом task
+        //то в следующем task при вызове next() выскакивает ошибка
+        //в инете пишут - это потому, что закрывается System.in
+        //и советуют один раз всего создавать scanner и закрывать в самом конце
+        //поэтому в каждый task его передаю
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exit = false;
+        do {
+            System.out.println();
+            System.out.println("Доступны следующие задачи:");
+            System.out.println("1. Угадай число (task1)");
+            System.out.println("2. Угадай слово (совпадение только по начальным символам) (task2)");
+            System.out.println("3. Угадай слово (совпадение по всем символам) (task2A)");
+            System.out.print("Введите номер задачи (0 - для выхода): ");
+            switch (readIntWithCheckBounds(scanner, 0, 3)) {
+                case 1 -> {
+                    //1. угадайка с трех попыток
+                    //на самом деле три попытки недостаточно для гарантированного угадывания
+                    task1(scanner, showSecret);
+
+                }
+                case 2 -> {
+                    //2. вариант реализации, когда совпадения ищутся только в начале
+                    //задуммного слова, до первого несовпадения
+                    task2(scanner, showSecret);
+
+                }
+                case 3 -> {
+                    //2. вариант реализации, когда совпадения ищутся на своих местах
+                    //по всему задуманному слову, а не только в его начале
+                    task2A(scanner, showSecret);
+
+                }
+                case 0 -> {
+                    exit = true;
+
+                }
+            }
+        } while (!exit);
+
+        scanner.close();
+    }
+
+    public static void task1(Scanner scanner, boolean showSecret) {
+        System.out.println("----------------- 1 -----------------");
+
+        do {
+            Random random = new Random();
+            int secret = random.nextInt(10);
+            int entered = -1;
+
+            String debugSecret = (showSecret) ? " (" + secret + ")" : "";
+
+            System.out.println();
+            System.out.printf("Программа \"задумала\" случайное число%s, у Вас есть ТРИ попытки, чтобы его угадать!\n", debugSecret);
+
+            for (int attempt = 1; attempt <= 3; attempt++) {
+                entered = readIntWithAttempt(attempt, scanner);
+                if (entered == secret) break;
+
+                System.out.printf("введенное число %d %s задуманного", entered, (entered < secret) ? "меньше" : "больше");
+                System.out.println();
+            }
+
+            if (entered == secret) System.out.println("ВЫ УГАДАЛИ ЧИСЛО!");
+            else System.out.printf("ВЫ ПРОИГРАЛИ! Было задумано число: %d...", secret);
+            System.out.println();
+
+            System.out.print("Повторить игру еще раз? 1 - да / 0 - нет: ");
+
+        } while (readIntWithCheckBounds(scanner, 0, 1) == 1);
+
+    }
+
+    //для task1
+    //выводит номер попытки, предлагает ввести число
+    public static int readIntWithAttempt(int attempt, Scanner scanner) {
+        System.out.printf("попытка %d из 3, введите число от %d до %d: ", attempt, 0, 9);
+        return readIntWithCheckBounds(scanner, 0, 9);
+    }
+
+    //для task1
+    //вводит число с проверкой диаппазона
+    //используется и для ввода угадываемого числа, и для ввода ответа 0/1
+    //и выбора номера задачи
+    public static int readIntWithCheckBounds(Scanner scanner, int start, int finish) {
+        int i;
+        while (true) {
+            i = scanner.nextInt();
+            if (i >= start && i <= finish) break;
+            System.out.printf("неправильное число %d, попробуйте еще: ", i);
+        }
+        return i;
+    }
+
+    public static void task2(Scanner scanner, boolean showSecret) {
+        System.out.println("----------------- 2 -----------------");
+
+        String[] words = createWords();
+        String secretWord = randomWord(words);
+
+        String debugSecret = (showSecret) ? " (" + secretWord + ")" : "";
+
+        System.out.println();
+        System.out.printf("Программа \"задумала\" одно слово%s из:\n", debugSecret);
+        outWords(words, 8);
+
+        while (true) {
+            System.out.print("Введите слово: ");
+            String answer = scanner.next();
+            String correct = correctChars(secretWord, answer);
+
+            if (correct.length() == secretWord.length() && correct.length() == answer.length()) {
+                System.out.println("ВЫ УГАДАЛИ СЛОВО!");
+                break;
+            }
+
+            if (correct.length() == 0) System.out.printf("%s - не совпало ни одной начальной буквы!", answer);
+            else System.out.printf("%s - совпало %s!", answer, padRight(correct, '#', 15));
+            System.out.println(" Попробуйте еще раз...");
+        }
+    }
+
+    //для task2 и task2A
+    //создание массива слов
+    public static String[] createWords() {
+        return new String[] {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+    }
+
+    //для task2 и task2A
+    //выбор случайного слова из массива
+    public static String randomWord(String[] words) {
+        Random random = new Random();
+        int secretIndex = random.nextInt(words.length);
+       // return words[secretIndex];
+        return words[new Random().nextInt(words.length)];
+    }
+
+    //для task2 и task2A
+    //вывод массива слов по wordsPerLine в одной строке
+    public static void outWords(String[] words, int wordsPerLine) {
+        if (wordsPerLine <= 0) wordsPerLine = 6;
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("  %s", words[i]);
+            if ((i + 1) % wordsPerLine == 0) System.out.println();
+        }
+        System.out.println();
+    }
+
+    //для task2 и task2A
+    //дополняет, если "надо", строку символами до нужной длины
+    public static String padRight(String s, char c, int len) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < len - s.length(); i++) sb.append(c);
+        return sb.toString();
+    }
+
+    //для task2
+    //ищет и возвращает первые совпадающие символы, до первого несовпадения
+    public static String correctChars(String secret, String answer) {
+        StringBuilder sb = new StringBuilder();
+        int len = minInt(secret.length(), answer.length());
+        for (int i = 0; i < len; i++) {
+            char c = secret.charAt(i);
+            if (c == answer.charAt(i)) sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    //для task2
+    //возвращает минимальное из двух целых
+    public static int minInt(int a, int b) {
+        if (a < b) return a;
+        else return b;
+    }
+
+    //вариант реализации второй задачи, когда совпадения ищутся на своих местах
+    //по всему задуманному слову, не только в его начале до первого несовпадение
+    public static void task2A(Scanner scanner, boolean showSecret) {
+        System.out.println("----------------- 2a -----------------");
+
+        String[] words = createWords();
+        String secretWord = randomWord(words);
+
+        String debugSecret = (showSecret) ? " (" + secretWord + ")" : "";
+
+        System.out.println();
+        System.out.printf("Программа \"задумала\" одно слово%s из:\n", debugSecret);
+        outWords(words, 8);
+
+        while (true) {
+            System.out.print("Введите слово: ");
+            String answer = scanner.next();
+
+            if (secretWord.equals(answer)) {
+                System.out.println("ВЫ УГАДАЛИ СЛОВО!");
+                break;
+            }
+
+            String matches = findMatches(secretWord, answer);
+            if (matches.length() != 0)
+                System.out.printf("%s - совпало %s!", answer, matches);
+            else
+                System.out.printf("%s - не совпало ни одной буквы!", answer);
+
+            System.out.println(" Попробуйте еще раз...");
+        }
+    }
+
+    //для task2A
+    //ищет и возвращает строку совпадений
+    //если не было ни одного, то возвращает пусту, иначе не понять, были совпадения или нет
+    public static String findMatches(String secret, String answer) {
+        boolean hasMatches = false;
+        StringBuilder matches = new StringBuilder();
+        for (int i = 0; i < secret.length(); i++) {
+            char c = secret.charAt(i);
+            if (i >= answer.length() || c != answer.charAt(i)) {
+                matches.append('#');
+            } else {
+                matches.append(c);
+                hasMatches = true;
+            }
+        }
+        if (!hasMatches) return "";
+        return padRight(matches.toString(), '#', 15);
+    }
+}
