e6d81b81302b8a381f76489835ef77f65ad64a95
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-02-13 17:46:45 +0300

    Исключение MyArraySizeException

---
+++ src/geeks/Lesson9/HW/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package geeks.Lesson9.HW;
+
+public class MyArraySizeException extends Exception{
+    MyArraySizeException(){
+        super("Массив превышает допустимое значение размерности массива");
+    };
+
+
+}
