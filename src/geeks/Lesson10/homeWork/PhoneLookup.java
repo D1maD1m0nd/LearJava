b8e4078542db4399ec80fae486bcef0003f3a4aa
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-02-16 02:24:17 +0300

    Предварительная версия хэш мар , где значение строка

--- src/geeks/Lesson10/homeWork/PhoneLookup.java
+++ src/geeks/Lesson10/homeWork/PhoneLookup.java
@@ -1,4 +1,27 @@
 package geeks.Lesson10.homeWork;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class PhoneLookup {
+
+    private Map persons;
+    PhoneLookup(){
+        persons = new HashMap();
+    }
+    //это надо переделать
+    public void add(String lastName, String phoneNumber){
+        if(persons.containsKey(lastName)){
+            //переделаю на коллекцию с строк
+            String newValue = persons.get(lastName).toString() + phoneNumber + " " ;
+            persons.put(lastName,newValue);
+            return;
+        }
+        persons.put(lastName, phoneNumber + " ");
+    }
+    public String get(String lastName){
+        return persons.get(lastName).toString();
+    }
 }
