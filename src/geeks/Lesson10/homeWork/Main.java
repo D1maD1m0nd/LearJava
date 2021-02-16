2db1caf86d9b4ff7e1c55f716a05d3aa81ac033f
your name <eqwe@yandex.ru>
2021-02-16 15:16:48 +0300

    10.2 изменение значения строк, на HashSet

--- src/geeks/Lesson10/homeWork/Main.java
+++ src/geeks/Lesson10/homeWork/Main.java
@@ -1,23 +1,17 @@
 package geeks.Lesson10.homeWork;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         //10.1
         //Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
-        List words = new ArrayList(Arrays.asList("orange", "apple", "ananas",
-                                                "phsphor", "ananas", "orange",
-                                                "potato", "coolity", "hility",
-                                                "potato", "potato", "seek",
-                                                "think", "about", "think"));
+        List words = new ArrayList(Arrays.asList("orange", "apple", "ananas", "phsphor", "ananas", "orange", "potato", "coolity", "hility", "potato", "potato", "seek", "think", "about", "think"));
         // Найти и вывести список уникальных слов,
         // из которых состоит массив (дубликаты не считаем).
         printOriginalWords(words);
         // Посчитать, сколько раз встречается каждое слово.
         countAllWords(words);
-
 
         //10.2
         //Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
@@ -42,29 +36,31 @@ public class Main {
 
     /**
      * Вывод всех уникальных слов
+     *
      * @param words - список уникальных слов
      */
-    public static void printOriginalWords(List words){
+    public static void printOriginalWords(List words) {
         System.out.println("Уникальные слова встречающиеся лишь один раз");
         for (var word: words) {
-            if(words.indexOf(word) == words.lastIndexOf(word)){
+            if (words.indexOf(word) == words.lastIndexOf(word)) {
                 System.out.println(word);
             }
         }
     }
-    public static void countAllWords(List words){
+
+    public static void countAllWords(List words) {
 
         System.out.println("Подсчет каждого слова");
         Set wordOriginal = new HashSet(words);
 
         for (var word: wordOriginal) {
-            if(words.indexOf(word) == words.lastIndexOf(word)){
+            if (words.indexOf(word) == words.lastIndexOf(word)) {
                 System.out.println(word + ": " + 1);
                 continue;
             }
             int countWord = 0;
-            for (var word1:words) {
-                if (word.equals(word1)){
+            for (var word1: words) {
+                if (word.equals(word1)) {
                     countWord++;
                 }
             }
--- src/geeks/Lesson10/homeWork/PhoneLookup.java
+++ src/geeks/Lesson10/homeWork/PhoneLookup.java
@@ -7,26 +7,29 @@ import java.util.Set;
 
 public class PhoneLookup {
 
-    private Map persons;
-    PhoneLookup(){
+    private final Map persons;
+
+    PhoneLookup() {
 
         persons = new HashMap();
     }
-    //это надо переделать
-    public void add(String lastName, String phoneNumber){
-        Set value = new HashSet();
-        if(persons.containsKey(lastName)){
 
-            value = (Set)persons.get(lastName);
+    //это надо переделать
+    public void add(String lastName, String phoneNumber) {
+        Set value = new HashSet();
+        if (persons.containsKey(lastName)) {
+
+            value = (Set) persons.get(lastName);
             value.add(phoneNumber);
-            persons.put(lastName,value);
+            persons.put(lastName, value);
             return;
         }
         value.add(phoneNumber);
         persons.put(lastName, value);
     }
-    public String get(String lastName){
-        if(persons.containsKey(lastName)){
+
+    public String get(String lastName) {
+        if (persons.containsKey(lastName)) {
             return persons.get(lastName).toString();
         }
         return "Человек с такой фамилией не найден";
