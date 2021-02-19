6b0ae347ecff392b89390476a187d24326c5b70d
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-02-19 23:11:39 +0300

    книга глава 5

---
+++ src/books/headfirst/Lesson5/GameHelper.java
@@ -0,0 +1,23 @@
+package books.headfirst.Lesson5;
+
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GameHelper {
+    public String getUserInput(String prompt) throws IOException {
+        String inputLine = null;
+        System.out.println(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if(inputLine.length() == 0){
+                return null;
+            }
+        }catch (IIOException e){
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
+}
---
+++ src/books/headfirst/Lesson5/SimpleDotComGame.java
@@ -0,0 +1,27 @@
+package books.headfirst.Lesson5;
+
+import java.io.IOException;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) throws IOException {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int)(Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive){
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkYourSelf(guess);
+            numOfGuesses++;
+            if(result.equals("Потопил")){
+                isAlive = false;
+                System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
+            }
+        }
+    }
+}
