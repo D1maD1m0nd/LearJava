ad65f681f9a0914ccfb9af0bf2151db2f5c39f0c
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-02-24 23:36:53 +0300

    Итоговый вариант 12

--- src/geeks/Lesson12/HomeWork/App.java
+++ src/geeks/Lesson12/HomeWork/App.java
@@ -1,6 +1,6 @@
 package geeks.Lesson12.HomeWork;
 
-public class Main {
+public class App {
     public static void main(String[] args) throws InterruptedException {
         ThreadArray newArray = new ThreadArray();
         //для однопотока
