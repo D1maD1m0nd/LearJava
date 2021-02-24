afc9d2c74a01cf7d234850c3df8482fc5e66f93d
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-02-24 14:49:34 +0300

    Итоговый вариант 12

--- src/geeks/Lesson12/HomeWork/Main.java
+++ src/geeks/Lesson12/HomeWork/Main.java
@@ -1,11 +1,11 @@
 package geeks.Lesson12.HomeWork;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadArray newArray = new ThreadArray();
         //для однопотока
         newArray.calcExpOneThread();
         //для многопотка
-        newArray.start();
+        newArray.calcExpInThreads();
     }
 }
--- src/geeks/Lesson12/HomeWork/ThreadArray.java
+++ src/geeks/Lesson12/HomeWork/ThreadArray.java
@@ -6,15 +6,6 @@ public class ThreadArray extends Thread {
     static final int SIZE = 10_000_000;
     static final int HALF = SIZE / 2;
 
-    @Override
-    public void run() {
-        try {
-            calcExpInThreads();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
     /**
      * Метод пересчитвает значения каждого элемента массива по формуле, массивом берется поле arr
      * (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
