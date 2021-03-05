9c5d4c6939d94678ef68cdde401cf5e34fe8c5d8
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-03-06 02:39:37 +0300

    Реализация начала гонки

--- src/geeks/Lessson13/HomeWork/Car.java
+++ src/geeks/Lessson13/HomeWork/Car.java
@@ -1,10 +1,17 @@
 package geeks.Lessson13.HomeWork;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+    public static int carsReady;
     public String getName() {
         return name;
     }
@@ -18,16 +25,22 @@ public class Car implements Runnable {
         this.name = "Участник #" + CARS_COUNT;
     }
     @Override
-    public void run() {
+    public void run(){
+
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            MainClass.cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println("Гонка началась");
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+
     }
 }
--- src/geeks/Lessson13/HomeWork/MainClass.java
+++ src/geeks/Lessson13/HomeWork/MainClass.java
@@ -2,9 +2,13 @@ package geeks.Lessson13.HomeWork;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
+    public static final CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
@@ -12,6 +16,7 @@ public class MainClass {
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
+
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
