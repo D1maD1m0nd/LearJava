4353b849c736dd8890441b9204fde12cf4888f5c
D1m0nd <d1m0hkrasav4ik@gmail.com>
2021-03-16 18:22:49 +0300

    1

--- src/geeks/Algorithms/Lesson3/HW/Main.java
+++ src/geeks/Algorithms/Lesson3/HW/Main.java
@@ -38,24 +38,38 @@ public class Main {
 
         //Реализуйте простой односвязный список и его базовые методы.
         MyLinkedListLine<String> myList = new MyLinkedListLine<>();
+
+        t1 = System.nanoTime();
         myList.insert("1232321");
         myList.insert("dsa");
         myList.insert("dsadsadas");
         myList.insert("dasdasdasdasdasd");
         myList.insert("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        myList.insert("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("Добавление элементов в одно список 6 элементов время  " + (System.nanoTime() - t1));
 
+        t1 = System.nanoTime();
+        myList.find("dsa");
+        System.out.println("Поиск односвяз время  " + (System.nanoTime() - t1));
         myList.display();
 
         //На основе списка из задания 3.1 реализуйте простой двусторонний список и его базовые методы.
         //Реализуйте список заполненный объектами из вашего класса из задания 1.3
-        MyLinkedList<String> list = new MyLinkedList<>();
 
+        MyLinkedList<String> list = new MyLinkedList<>();
+        t1 = System.nanoTime();
         list.insert("Artom");
         list.insert("Rim");
         list.insert("Vasya");
         list.insert("Tema");
         list.insert("Max");
         list.insert("Vasek");
+        System.out.println("Добавление элементов в двусвязный список 6 элементов время  " + (System.nanoTime() - t1));
+
+
+        t1 = System.nanoTime();
+        list.find("Max");
+        System.out.println("поиск элементов в двусвязный список   время  " + (System.nanoTime() - t1));
 
         System.out.println(list.find("Max"));
         System.out.println(list.find("Rim"));
